23101b8a59437fce4b5d74744fad445e48059468
b-sakai <blueberryoctopas@gmail.com>
2024-02-04 10:33:54 +0900

    add cumulative pages, change x-axis for date, add chartjs-adapter-moment

--- src/bookmeterHistryGraph.js
+++ src/bookmeterHistryGraph.js
@@ -1,31 +1,66 @@
 import React from 'react';
-import bookData from './python/book_data.json';
 import { Line } from 'react-chartjs-2';
-import { Chart, registerables } from "chart.js"
-Chart.register(...registerables)
+import { Chart, registerables } from 'chart.js';
+import bookData from './python/book_data.json';
+import 'chartjs-adapter-moment';
 
+Chart.register(...registerables);
+
+const calculateCumulative = (data) => {
+  let cumulative = 0;
+  return data.map((value) => {
+    const intValue = parseInt(value, 10);
+    cumulative += intValue;
+    return cumulative;
+  });
+};
+
+const parseDate = (dateString) => new Date(dateString);
 
 const BookmeterChart = () => {
+  const cumulativePages = calculateCumulative(bookData.pages);
+
   const chartData = {
-    labels: bookData.dates,
+    labels: bookData.dates.map(parseDate),
     datasets: [
       {
+        label: 'Pages',
+        data: bookData.pages.map((value) => parseInt(value, 10)),
+        borderColor: 'green',
+        backgroundColor: 'rgba(0, 255, 0, 0.5)',
+      },
+      {
         label: 'Cumulative Pages',
-        data: bookData.pages,
+        data: cumulativePages,
         borderColor: 'blue',
         backgroundColor: 'rgba(0, 0, 255, 0.5)',
       },
     ],
   };
 
+  const options = {
+    scales: {
+      x: {
+        type: 'time',
+        time: {
+          unit: 'day',
+          tooltipFormat: 'YYYY/MM/DD', // ツールチップに表示される日付のフォーマット
+        },
+      },
+      y: {
+        beginAtZero: true,
+      },
+    },
+  };
+
   return (
     <div>
-      <h2>Cumulative Pages Chart</h2>
+      <h2>Bookmeter Chart</h2>
       <div style={{ width: '80%', margin: 'auto' }}>
-        <Line data={chartData} />
+        <Line data={chartData} options={options} />
       </div>
     </div>
   );
 };
 
-export default BookmeterChart
+export default BookmeterChart;
